10e3764f7194438d39e6c7d71a547e47c96a3d08
endlessl8p <104884878+endlessl8p@users.noreply.github.com>
2022-08-19 22:37:29 -0700

    Create ci.yml (#1)
    * Create ci.yml
    
    * Update Podfile
    
    * Update app id and add ios and android commands
    
    * Update ci.yml
    
    * Remove Install Podfiles
    
    * Update App.tsx
    
    * Add setup Java
    
    * hide some commands

--- example/android/app/src/main/java/com/example/reactnativereactnativewalletcore/newarchitecture/components/MainComponentsRegistry.java
+++ example/android/app/src/main/java/com/example/reactnativereactnativewalletcore/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.example.reactnativereactnativewalletcore.newarchitecture.components;
+package org.magicwallet.reactnativereactnativewalletcore.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
