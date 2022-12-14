559ce42b79796d36a7672d4dde0d9d574d19b3ae
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2022-12-14 13:31:13 +0800

    Add Hash namespace and expose sha3/256

--- ios/ReactNativeWalletCore.swift
+++ ios/ReactNativeWalletCore.swift
@@ -23,8 +23,46 @@ class ReactNativeWalletCore: NSObject {
         resolve(wallet.mnemonic)
     }
     
+    @objc(sha3_256:withResolver:withRejecter:)
+    func sha3_256(string: String, resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
+        guard let data = stringToData(string: string) else {
+            reject(Self.errorDomain, "invalid hex string", nil)
+            return
+        }
+        resolve(Hash.sha3_256(data: data).hexString)
+    }
+    
+    @objc(sha256:withResolver:withRejecter:)
+    func sha256(string: String, resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
+        guard let data = stringToData(string: string) else {
+            reject(Self.errorDomain, "invalid hex string", nil)
+            return
+        }
+        resolve(Hash.sha256(data: data).hexString)
+    }
+    
     @objc(sha512:withResolver:withRejecter:)
-    func sha512(string: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(Hash.sha512(data: string.data(using: .utf8)!).hexString)
+    func sha512(string: String, resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
+        guard let data = stringToData(string: string) else {
+            reject(Self.errorDomain, "invalid hex string", nil)
+            return
+        }
+        resolve(Hash.sha512(data: data).hexString)
+    }
+    
+    @objc(keccak256:withResolver:withRejecter:)
+    func keccak256(string: String, resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
+        guard let data = stringToData(string: string) else {
+            reject(Self.errorDomain, "invalid hex string", nil)
+            return
+        }
+        resolve(Hash.keccak256(data: data).hexString)
+    }
+    
+    func stringToData(string: String) -> Data? {
+        if string.hasPrefix("0x") {
+            return Data(hexString: string)
+        }
+        return Data(string.utf8)
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -15,19 +15,40 @@ const ReactNativeWalletCore = NativeModules.ReactNativeWalletCore ? NativeModule
   }
 );
 
-export function validateAddress(address: string, coin: number): Promise<boolean> {
-  return ReactNativeWalletCore.validateAddress(address, coin);
+export namespace AnyAddress {
+  export function validateAddress(address: string, coin: number): Promise<boolean> {
+    return ReactNativeWalletCore.validateAddress(address, coin);
+  }
 }
 
-export function createWallet(
-  strength: number,
-  passphrase: string
-): Promise<string> {
-  return ReactNativeWalletCore.createWallet(strength, passphrase);
+export namespace HDWallet {
+  export function createWallet(
+    strength: number,
+    passphrase: string
+  ): Promise<string> {
+    return ReactNativeWalletCore.createWallet(strength, passphrase);
+  }
 }
 
-export function sha512(
-  value: string
-): Promise<string> {
-  return ReactNativeWalletCore.sha512(value);
+export namespace Hash {
+  export function sha3_256(
+    value: string
+  ): Promise<string> {
+    return ReactNativeWalletCore.sha3_256(value);
+  }
+  export function sha256(
+    value: string
+  ): Promise<string> {
+    return ReactNativeWalletCore.sha256(value);
+  }
+  export function keccak256(
+    value: string
+  ): Promise<string> {
+    return ReactNativeWalletCore.keccak256(value);
+  }
+  export function sha512(
+    value: string
+  ): Promise<string> {
+    return ReactNativeWalletCore.sha512(value);
+  }
 }
