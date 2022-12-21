dd5fbc69a0cdf4eef1bee0231de0653049c7fbc0
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2022-12-21 15:36:58 +0800

    Add lint:swift

--- ios/ReactNativeWalletCore.swift
+++ ios/ReactNativeWalletCore.swift
@@ -4,9 +4,9 @@ import WalletCore
 class ReactNativeWalletCore: NSObject {
 
     static let errorDomain = "Wallet Core Error"
-    
+
     @objc(validateAddress:withCoin:withResolver:withRejecter:)
-    func validateAddress(address: String, coin: NSNumber, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) {
+    func validateAddress(address: String, coin: NSNumber, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
         guard let coinType = CoinType(rawValue: UInt32(coin.int32Value)) else {
             reject(Self.errorDomain, "coin: \(coin) is not a valid CoinType", nil)
             return
@@ -15,7 +15,7 @@ class ReactNativeWalletCore: NSObject {
     }
 
     @objc(createWallet:withPassphrase:withResolver:withRejecter:)
-    func createWallet(strength: NSNumber, passphrase: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) {
+    func createWallet(strength: NSNumber, passphrase: String, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
         guard let wallet = HDWallet(strength: strength.int32Value, passphrase: passphrase) else {
             reject(Self.errorDomain, "failed to create HDWallet", nil)
             return
